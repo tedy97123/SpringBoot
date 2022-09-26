4cb37f79a3fb973b4ab461a2197a4ff277d356c7
Tedy Yohanes <91521891+tedy97123@users.noreply.github.com>
2022-09-26 03:46:23 -0700

    Update with CRUD portion
    add flesh onto these files ...
    UserRepository,  ResourceNotFound, UserController

--- spring-crud-restful/src/main/java/net/javaguides/springboot/SpringCrudRestfulApplication.java
+++ spring-crud-restful/src/main/java/net/javaguides/springboot/SpringCrudRestfulApplication.java
@@ -3,6 +3,8 @@ package net.javaguides.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//entry point of the project
+
 @SpringBootApplication
 public class SpringCrudRestfulApplication {
 
